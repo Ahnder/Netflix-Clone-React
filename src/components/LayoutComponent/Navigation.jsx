7083df541f1b678b5294e4cff1f3a5c22772c34e
Ahnder <skguss1@gmail.com>
2020-06-05 19:08:43 +0900

    add func(Navigation.jsx) - scrolldown && change navigation background color

--- src/components/LayoutComponent/Navigation.jsx
+++ src/components/LayoutComponent/Navigation.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { FiSearch, FiGift, FiBell, FiSmile } from 'react-icons/fi';
 
 import './Navigation.scss';
@@ -6,9 +6,22 @@ import './Navigation.scss';
 import navigationLeftMenuLogo from '../../static/images/Netflix_Logo.png';
 
 const Navigation = () => {
+  const [scrollNav, setScrollNav] = useState(false);
+
+  const handleScrollNav = () => {
+    window.scrollY === 0
+      ? setScrollNav(false)
+      : window.scrollY > 50 && setScrollNav(true);
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScrollNav);
+    return () => window.removeEventListener('scroll', handleScrollNav);
+  }, []);
+
   return (
     <nav>
-      <div className="navigationContainer">
+      <div className={'navigationContainer ' + (scrollNav && 'scrollDown')}>
         <div className="leftMenu">
           <img src={navigationLeftMenuLogo} alt="navigationLogo" />
           <div className="link">Home</div>
